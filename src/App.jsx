f4e39c6baa319bcf16c8e6110b2ca086c65f1daf
nutan kumari <nutananju240@gmail.com>
2023-12-28 14:58:09 +0530

    counter app '

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [data, setData] = useState(0);
 
+  const update = () => {
+    return setData(data + 1);
+  }
+  const dec = () => {
+    if (data > 0) {
+      setData(data - 1);
+    }
+  }
   return (
-    <h1 className="text-3xl font-bold underline text-fuchsia-500	">
-    Hello world!
-  </h1> 
+    <>
+      <p>{data}</p>
+      <div className='flex '>
+        <button className='border-black border-2 px-6 m-2' onClick={update}>increase</button><br />
+        <button className={data==0 ? 'border-black border-2 px-6 text-slate-200 m-2':'border-black border-2 px-6 m-2'} disabled={data == 0} onClick={dec}>decrease</button>
+      </div>
+    </>
   )
 }
 
