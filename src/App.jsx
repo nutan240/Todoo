d0db551282523d090d55dede8ccbecaed0c1c671
nutan kumari <nutananju240@gmail.com>
2023-12-28 18:01:13 +0530

    todo ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,53 @@
-import './App.css'
+import React, { useEffect, useState } from 'react';
+import './App.css';
 
 function App() {
+  const [input, setInput] = useState('');
+  const [data, setData] = useState([]);
+
+  const handleAddTodo = (e) => {
+    e.preventDefault();
+    setData([...data, { input }]);
+    setInput('');
+  };
+  const handleOnChange = (e) => {
+    setInput(e.target.value);
+  };
 
   return (
-    <h1 className="text-3xl font-bold underline text-fuchsia-500	">
-    Hello world!
-  </h1> 
-  )
+    <>
+      {data.length > 0 && <div className='px-5 my-7' >
+        <h1 className='font-medium text-lg'>Todo App</h1>
+        {
+          data.map((e, i) => {
+            return (<p className='border-2 rounded px-2 my-4' key={i} >
+              <input className='mx-2' type='checkbox' />
+              {e.input}
+            </p>
+            )
+          })}
+      </div>
+      }
+      <form className='mx-5 my-7' onSubmit={(e) => handleAddTodo(e)}>
+        <p>Todo</p>
+        <input
+          className='border-2 rounded my-1 w-full focus:outline-none'
+          type='text'
+          value={input} 
+          onChange={(e) => handleOnChange(e)}
+          placeholder='your task...'
+        />
+        <br />
+        <button
+          className='my-1 px-2 border-2 rounded'
+          type='submit'
+          onClick={handleAddTodo} 
+        >
+          Submit
+        </button>
+      </form>
+    </>
+  );
 }
 
-export default App
+export default App;
