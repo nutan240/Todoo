7d886f93391a3afe7e385e0ee4fda3c1a308a8cd
nutan kumari <nutananju240@gmail.com>
2023-12-28 15:33:25 +0530

    change function name

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,24 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [data, setData] = useState(0);
+  const [counter, setCounter] = useState(0);
 
-  const update = () => {
-    return setData(data + 1);
+  const increasevalue = () => {
+    return setCounter(counter + 1);
   }
-  const dec = () => {
-    if (data > 0) {
-      setData(data - 1);
+  const decreasevalue = () => {
+    if (counter > 0) {
+      setCounter(counter - 1);
     }
   }
   return (
     <>
-      <p>{data}</p>
-      <div className='flex '>
-        <button className='border-black border-2 px-6 m-2' onClick={update}>increase</button><br />
-        <button className={data==0 ? 'border-black border-2 px-6 text-slate-200 m-2':'border-black border-2 px-6 m-2'} disabled={data == 0} onClick={dec}>decrease</button>
+      <div className='items-center bg-indigo-100  '>
+        <p className='m-4'>{counter}</p>
+        <div className='flex '>
+          <button className='border-black border-2 px-6 m-2 bg-sky-500/75 rounded-md	' onClick={increasevalue}>Increase</button><br />
+          <button className={counter == 0 ? 'border-black border-2 px-6 text-slate-200 m-2' : 'border-black border-2 px-6 m-2 bg-sky-500/75 rounded-md	'} disabled={counter == 0} onClick={decreasevalue}>Decrease</button>
+        </div>
       </div>
     </>
   )
