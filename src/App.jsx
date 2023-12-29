485b2f708a5d73db215ddf2443a6b3c19ca4f634
nutan kumari <nutananju240@gmail.com>
2023-12-29 16:40:53 +0530

    adding delete operation

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,61 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [input, setInput] = useState('');
   const [data, setData] = useState([]);
+  const [input, setInput] = useState('');
 
   const handleAddTodo = (e) => {
     e.preventDefault();
-    setData([...data, { input }]);
+    setData([...data, { name: input, id: Math.random() * 10 }]);
     setInput('');
   };
+
   const handleOnChange = (e) => {
     setInput(e.target.value);
   };
 
+  const handleDelete = (index) => {
+    const newdata = [...data];
+    newdata.splice(index, 1);
+    setData(newdata);
+    setInput('');
+  };
+  const handleCheckboxChange = (index) => {
+    const newdata = [...data];
+    newdata[index].completed = !newdata[index].completed;
+    setData(newdata);
+  };
   return (
     <>
-      {data.length > 0 && <div className='px-5 my-7' >
-        <h1 className='font-medium text-lg'>Todo App</h1>
-        {
-          data.map((e, i) => {
-            return (<p className='border-2 rounded px-2 my-4' key={i} >
-              <input className='mx-2' type='checkbox' />
-              {e.input}
+      {data.length > 0 && (
+        <div className='ml-5  mt-7 mb-0 mr-10 '>
+          <h1 className='font-medium font-mono text-xl mb-1'>Todo App</h1>
+          {data.map((e, index) => (
+            <p className='border-2 bg-white  rounded pl-2 pr-4 py-1 mb-4 mt-1 w-full ' key={index}>
+            <div>
+              <input className='mx-2' type='checkbox'   checked={e.completed} onChange={() => handleCheckboxChange(index)}/>
+              {e.name}
+              </div>
+              <span className='icons'>
+                <i className="fa-solid fa-trash-can" onClick={() => handleDelete(index)}></i>
+                {e.completed && <button className='my-1 ml-3 px-2 border-2 rounded ' type='submit'>Completed</button>}
+              </span>
             </p>
-            )
-          })}
-      </div>
-      }
-      <form className='mx-5 my-7' onSubmit={(e) => handleAddTodo(e)}>
+          ))}
+        </div>
+      )}
+      <form className=' mb-7 pl-4 mt-0  mr-10 ' onSubmit={(e) => handleAddTodo(e)}>
         <p>Todo</p>
         <input
           className='border-2 rounded my-1 w-full focus:outline-none'
           type='text'
-          value={input} 
+          value={input}
           onChange={(e) => handleOnChange(e)}
           placeholder='your task...'
         />
         <br />
-        <button
-          className='my-1 px-2 border-2 rounded'
-          type='submit'
-          onClick={handleAddTodo} 
-        >
+        <button className='my-1 px-2 border-2 rounded ' type='submit' onClick={handleAddTodo}>
           Submit
         </button>
       </form>
