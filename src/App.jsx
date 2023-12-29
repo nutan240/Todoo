7c2b363d9ee6305f04c57fdaba49a2883b818e01
nutan kumari <nutananju240@gmail.com>
2023-12-29 17:46:13 +0530

    delete functionalities with id

--- src/App.jsx
+++ src/App.jsx
@@ -7,34 +7,34 @@ function App() {
 
   const handleAddTodo = (e) => {
     e.preventDefault();
+    if(input!=='')
     setData([...data, { name: input, id: Math.random() * 10 }]);
     setInput('');
   };
-
   const handleOnChange = (e) => {
     setInput(e.target.value);
   };
-
-  const handleDelete = (index) => {
+  const handleDelete = (id) => {
     const newdata = [...data];
-    newdata.splice(index, 1);
+    newdata.splice(id, 1);
     setData(newdata);
     setInput('');
   };
   const handleCheckboxChange = (index) => {
     const newdata = [...data];
+    console.log(newdata , data)
     newdata[index].completed = !newdata[index].completed;
     setData(newdata);
   };
   return (
     <>
       {data.length > 0 && (
-        <div className='ml-5  mt-7 mb-0 mr-10 '>
+        <div className='ml-5  mt-7  mb-0 mr-10 '>
           <h1 className='font-medium font-mono text-xl mb-1'>Todo App</h1>
           {data.map((e, index) => (
-            <p className='border-2 bg-white  rounded pl-2 pr-4 py-1 mb-4 mt-1 w-full ' key={index}>
+            <p className='border-2 bg-white flex justify-between rounded pl-2 pr-4 py-1 mb-4 mt-1 w-full ' key={index}>
             <div>
-              <input className='mx-2' type='checkbox'   checked={e.completed} onChange={() => handleCheckboxChange(index)}/>
+              <input className='mx-2' type='checkbox' checked={e.completed} onChange={() => handleCheckboxChange(index)}/>
               {e.name}
               </div>
               <span className='icons'>
@@ -45,7 +45,7 @@ function App() {
           ))}
         </div>
       )}
-      <form className=' mb-7 pl-4 mt-0  mr-10 ' onSubmit={(e) => handleAddTodo(e)}>
+      <form className=' mb-7 pl-4 mt-0 mr-10 ' onSubmit={(e) => handleAddTodo(e)}>
         <p>Todo</p>
         <input
           className='border-2 rounded my-1 w-full focus:outline-none'
