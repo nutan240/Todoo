2757221cfe904db2b9375f93cb9ecc74327ca631
nutan kumari <nutananju240@gmail.com>
2024-01-03 16:39:01 +0530

    useing  appropriate state name

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [data, setData] = useState([]);
+  const [tododata, setTododata] = useState([]);
   const [input, setInput] = useState('');
   const [edit, setEdit] = useState(null);
   const handleAddTodo = (e) => {
@@ -10,45 +10,45 @@ function App() {
     if (input !== '') {
       if (edit !== null) {
         const id = edit[0].id;
-        setData(data.map((e) => (e.id === id ? { ...e, name: input } : e)));
+        setTododata(tododata.map((e) => (e.id === id ? { ...e, name: input } : e)));
         setEdit(null);
       } else {
-        const newData = [
-          ...data,
+        const newdata = [
+          ...tododata,
           { name: input, id: Math.floor(Math.random() * 10), check: false },
         ];
-        setData(newData);
+        setTododata(newdata);
       }
       setInput('');
     }
   };
-  console.log(data);
+  console.log(tododata);
   const handleOnChange = (e) => {
     setInput(e.target.value);
   };
   const handleEdit = (id) => {
-    const newdata = data;
+    const newdata = tododata;
     const value = newdata.filter((item) => item.id === id);
     setInput(value[0].name);
     setEdit(value);
   };
   const handleDelete = (id) => {
     console.log(id);
-    const newdata = [...data];
-    const s =newdata.filter((value)=>value.id!==id)
-    setData(s);
+    const newdata = [...tododata];
+    const value =newdata.filter((value)=>value.id!==id)
+    setTododata(value);
     setInput('');
   };
   const handleCheckboxChange = (id) => {
-    setData(data.map((e)=>e.id===id?{...e,check:!e.check}:e))
+    setTododata(tododata.map((e)=>e.id===id?{...e,check:!e.check}:e))
   };
   return (
     <>
     <div className='w-screen min-h-screen'>
-      {data.length > 0 && (
+      {tododata.length > 0 && (
         <div className='ml-5  mt-7  mb-0 mr-10 w-98 '>
           <h1 className='font-medium font-mono text-xl mb-1'>Todo App</h1>
-          {data.map((e) => (
+          {tododata.map((e) => (
             <div className='border-2 bg-white flex justify-between rounded pl-2 pr-4 py-1 mb-2 mt-1 text-overflow-ellipsis 		' key={e.id}>
             <div>
               <input className='mx-2' type='checkbox' checked={e.check} onClick={() => handleCheckboxChange(e.id)}/>
@@ -67,10 +67,10 @@ function App() {
           ))}
         </div>
       )}
-      <form className=' mb-7 pl-4 mt-0 mr-10 text-overflow-ellipsis overflow-hidden whitespace-nowrap ' onSubmit={(e) => handleAddTodo(e)}>
+      <form className=' mb-7 pl-4 mt-0 mr-10  whitespace-nowrap ' onSubmit={(e) => handleAddTodo(e)}>
         <p>Todo</p>
         <textarea
-          className='border-2 rounded my-1 w-full focus:outline-none h-auto'
+          className='border-2 rounded my-1 w-full focus:outline-none min-h-3'
           type='text'
           value={input}
           onChange={(e) => handleOnChange(e)}
